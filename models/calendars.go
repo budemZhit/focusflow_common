fffdd8c75690519c4a3c76ee3b3fbdb0b353c4a8
alexei <guriev@dk-lab.ru>
2025-12-25 15:33:29 +0300

    refactore: move models to separete module

--- models/calendars.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Calendars struct {
-	ID          int64  `db:"id" json:"id"`
-	OwnerID     int64  `db:"owner_id" json:"owner_id"`
-	Name        string `db:"name" json:"name"`
-	Description string `db:"description" json:"description"`
-}
